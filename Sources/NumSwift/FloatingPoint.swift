8a54c8914db6bb4be845b8d783f8cfa6d1875b2e
William Vabrinskas <williamvabrinskas@mbp14inch2021.lan>
2022-04-27 18:36:43 -0400

    added 3d array math

--- Sources/NumSwift/FloatingPoint.swift
+++ Sources/NumSwift/FloatingPoint.swift
@@ -10,94 +10,6 @@ import Accelerate
 
 public extension Array where Element == [Double] {
   
-  static func *(lhs: Self, rhs: Self) -> Self {
-    let left = lhs
-    let right = rhs
-    
-    let leftShape = left.shape
-    let rightShape = right.shape
-    
-    precondition(leftShape == rightShape)
-    
-    let depth = leftShape[safe: 2] ?? 0
-    let rows = leftShape[safe: 1] ?? 0
-    
-    var result: Self = []
-    for d in 0..<depth {
-      for r in 0..<rows {
-        result[d][r] = left[d][r] * right[d][r]
-      }
-    }
-    
-    return result
-  }
-  
-  static func /(lhs: Self, rhs: Self) -> Self {
-    let left = lhs
-    let right = rhs
-    
-    let leftShape = left.shape
-    let rightShape = right.shape
-    
-    precondition(leftShape == rightShape)
-    
-    let depth = leftShape[safe: 2] ?? 0
-    let rows = leftShape[safe: 1] ?? 0
-    
-    var result: Self = []
-    for d in 0..<depth {
-      for r in 0..<rows {
-        result[d][r] = left[d][r] / right[d][r]
-      }
-    }
-    
-    return result
-  }
-  
-  static func -(lhs: Self, rhs: Self) -> Self {
-    let left = lhs
-    let right = rhs
-    
-    let leftShape = left.shape
-    let rightShape = right.shape
-    
-    precondition(leftShape == rightShape)
-    
-    let depth = leftShape[safe: 2] ?? 0
-    let rows = leftShape[safe: 1] ?? 0
-    
-    var result: Self = []
-    for d in 0..<depth {
-      for r in 0..<rows {
-        result[d][r] = left[d][r] - right[d][r]
-      }
-    }
-    
-    return result
-  }
-  
-  static func +(lhs: Self, rhs: Self) -> Self {
-    let left = lhs
-    let right = rhs
-    
-    let leftShape = left.shape
-    let rightShape = right.shape
-    
-    precondition(leftShape == rightShape)
-    
-    let depth = leftShape[safe: 2] ?? 0
-    let rows = leftShape[safe: 1] ?? 0
-    
-    var result: Self = []
-    for d in 0..<depth {
-      for r in 0..<rows {
-        result[d][r] = left[d][r] + right[d][r]
-      }
-    }
-    
-    return result
-  }
-  
   func zeroPad(filterSize: (Int, Int), stride: (Int, Int) = (1,1)) -> Self {
     guard let first = self.first else {
       return self
@@ -199,95 +111,7 @@ public extension Array where Element == [Double] {
 }
 
 public extension Array where Element == [Float] {
-  
-  static func *(lhs: Self, rhs: Self) -> Self {
-    let left = lhs
-    let right = rhs
-    
-    let leftShape = left.shape
-    let rightShape = right.shape
-    
-    precondition(leftShape == rightShape)
-    
-    let depth = leftShape[safe: 2] ?? 0
-    let rows = leftShape[safe: 1] ?? 0
-    
-    var result: Self = []
-    for d in 0..<depth {
-      for r in 0..<rows {
-        result[d][r] = left[d][r] * right[d][r]
-      }
-    }
-    
-    return result
-  }
-  
-  static func /(lhs: Self, rhs: Self) -> Self {
-    let left = lhs
-    let right = rhs
-    
-    let leftShape = left.shape
-    let rightShape = right.shape
-    
-    precondition(leftShape == rightShape)
-    
-    let depth = leftShape[safe: 2] ?? 0
-    let rows = leftShape[safe: 1] ?? 0
-    
-    var result: Self = []
-    for d in 0..<depth {
-      for r in 0..<rows {
-        result[d][r] = left[d][r] / right[d][r]
-      }
-    }
-    
-    return result
-  }
-  
-  static func -(lhs: Self, rhs: Self) -> Self {
-    let left = lhs
-    let right = rhs
-    
-    let leftShape = left.shape
-    let rightShape = right.shape
-    
-    precondition(leftShape == rightShape)
-    
-    let depth = leftShape[safe: 2] ?? 0
-    let rows = leftShape[safe: 1] ?? 0
-    
-    var result: Self = []
-    for d in 0..<depth {
-      for r in 0..<rows {
-        result[d][r] = left[d][r] - right[d][r]
-      }
-    }
-    
-    return result
-  }
-  
-  static func +(lhs: Self, rhs: Self) -> Self {
-    let left = lhs
-    let right = rhs
-    
-    let leftShape = left.shape
-    let rightShape = right.shape
-    
-    precondition(leftShape == rightShape)
-    
-    let depth = leftShape[safe: 2] ?? 0
-    let rows = leftShape[safe: 1] ?? 0
-    
-    var result: Self = []
-    for d in 0..<depth {
-      for r in 0..<rows {
-        result[d][r] = left[d][r] + right[d][r]
-      }
-    }
-    
-    return result
-  }
-  
+
   func zeroPad(padding: NumSwiftPadding) -> Self {
     guard let first = self.first else {
       return self
@@ -947,5 +771,186 @@ public extension Array where Element: Equatable & Numeric & FloatingPoint {
   
   static func -(lhs: Element, rhs: [Element]) -> [Element] {
     return rhs.map({ lhs - $0 })
+  }
+}
+
+public extension Array where Element == [[Double]] {
+  static func *(lhs: Self, rhs: Self) -> Self {
+    let left = lhs
+    let right = rhs
+    
+    let leftShape = left.shape
+    let rightShape = right.shape
+    
+    precondition(leftShape == rightShape)
+    
+    let depth = leftShape[safe: 2] ?? 0
+    let rows = leftShape[safe: 1] ?? 0
+    
+    var result: Self = []
+    for d in 0..<depth {
+      for r in 0..<rows {
+        result[d][r] = left[d][r] * right[d][r]
+      }
+    }
+    
+    return result
+  }
+  
+  static func /(lhs: Self, rhs: Self) -> Self {
+    let left = lhs
+    let right = rhs
+    
+    let leftShape = left.shape
+    let rightShape = right.shape
+    
+    precondition(leftShape == rightShape)
+    
+    let depth = leftShape[safe: 2] ?? 0
+    let rows = leftShape[safe: 1] ?? 0
+    
+    var result: Self = []
+    for d in 0..<depth {
+      for r in 0..<rows {
+        result[d][r] = left[d][r] / right[d][r]
+      }
+    }
+    
+    return result
+  }
+  
+  static func -(lhs: Self, rhs: Self) -> Self {
+    let left = lhs
+    let right = rhs
+    
+    let leftShape = left.shape
+    let rightShape = right.shape
+    
+    precondition(leftShape == rightShape)
+    
+    let depth = leftShape[safe: 2] ?? 0
+    let rows = leftShape[safe: 1] ?? 0
+    
+    var result: Self = []
+    for d in 0..<depth {
+      for r in 0..<rows {
+        result[d][r] = left[d][r] - right[d][r]
+      }
+    }
+    
+    return result
+  }
+  
+  static func +(lhs: Self, rhs: Self) -> Self {
+    let left = lhs
+    let right = rhs
+    
+    let leftShape = left.shape
+    let rightShape = right.shape
+    
+    precondition(leftShape == rightShape)
+    
+    let depth = leftShape[safe: 2] ?? 0
+    let rows = leftShape[safe: 1] ?? 0
+    
+    var result: Self = []
+    for d in 0..<depth {
+      for r in 0..<rows {
+        result[d][r] = left[d][r] + right[d][r]
+      }
+    }
+    
+    return result
+  }
+}
+
+
+public extension Array where Element == [[Float]] {
+  static func *(lhs: Self, rhs: Self) -> Self {
+    let left = lhs
+    let right = rhs
+    
+    let leftShape = left.shape
+    let rightShape = right.shape
+    
+    precondition(leftShape == rightShape)
+    
+    let depth = leftShape[safe: 2] ?? 0
+    let rows = leftShape[safe: 1] ?? 0
+    
+    var result: Self = []
+    for d in 0..<depth {
+      for r in 0..<rows {
+        result[d][r] = left[d][r] * right[d][r]
+      }
+    }
+    
+    return result
+  }
+  
+  static func /(lhs: Self, rhs: Self) -> Self {
+    let left = lhs
+    let right = rhs
+    
+    let leftShape = left.shape
+    let rightShape = right.shape
+    
+    precondition(leftShape == rightShape)
+    
+    let depth = leftShape[safe: 2] ?? 0
+    let rows = leftShape[safe: 1] ?? 0
+    
+    var result: Self = []
+    for d in 0..<depth {
+      for r in 0..<rows {
+        result[d][r] = left[d][r] / right[d][r]
+      }
+    }
+    
+    return result
+  }
+  
+  static func -(lhs: Self, rhs: Self) -> Self {
+    let left = lhs
+    let right = rhs
+    
+    let leftShape = left.shape
+    let rightShape = right.shape
+    
+    precondition(leftShape == rightShape)
+    
+    let depth = leftShape[safe: 2] ?? 0
+    let rows = leftShape[safe: 1] ?? 0
+    
+    var result: Self = []
+    for d in 0..<depth {
+      for r in 0..<rows {
+        result[d][r] = left[d][r] - right[d][r]
+      }
+    }
+    
+    return result
+  }
+  
+  static func +(lhs: Self, rhs: Self) -> Self {
+    let left = lhs
+    let right = rhs
+    
+    let leftShape = left.shape
+    let rightShape = right.shape
+    
+    precondition(leftShape == rightShape)
+    
+    let depth = leftShape[safe: 2] ?? 0
+    let rows = leftShape[safe: 1] ?? 0
+    
+    var result: Self = []
+    for d in 0..<depth {
+      for r in 0..<rows {
+        result[d][r] = left[d][r] + right[d][r]
+      }
+    }
+    
+    return result
   }
 }

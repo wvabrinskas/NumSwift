2ba33636a0cf0dec5ac3b92e788ce0e582f4a429
William Vabrinskas <williamvabrinskas@mbp14inch2021.lan>
2022-05-24 15:42:42 -0400

    added zero pad two 2d array

--- Sources/NumSwift/NumSwiftC.swift
+++ Sources/NumSwift/NumSwiftC.swift
@@ -64,6 +64,41 @@ public struct NumSwiftC {
     return results.reshape(columns: expectedColumns)
   }
   
+  public static func zeroPad(signal: [[Float]],
+                             filterSize: (rows: Int, columns: Int),
+                             inputSize: (rows: Int, columns: Int),
+                             stride: (Int, Int) = (1,1)) -> [[Float]] {
+    
+    let padding = NumSwiftC.paddingCalculation(strides: stride,
+                                               padding: .same,
+                                               filterSize: filterSize,
+                                               inputSize: inputSize)
+    
+    guard padding.right > 0 || padding.left > 0 || padding.top > 0 || padding.bottom > 0 else {
+      return signal
+    }
+    
+    let shape = signal.shape
+    let rows = shape[safe: 1, 0]
+    let columns = shape[safe: 0, 0]
+    
+    let expectedRows = rows + padding.top + padding.bottom
+    let expectedColumns = columns + padding.left + padding.right
+    var results: [Float] = [Float](repeating: 0, count: expectedRows * expectedColumns)
+
+    let flatSignal: [Float] = signal.flatten()
+    nsc_specific_zero_pad(flatSignal,
+                          &results,
+                          NSC_Size(rows: Int32(rows),
+                                   columns: Int32(columns)),
+                          Int32(padding.top),
+                          Int32(padding.bottom),
+                          Int32(padding.left),
+                          Int32(padding.right))
+    
+    return results.reshape(columns: expectedColumns)
+  }
+  
   public static func conv2d(signal: [Float],
                             filter: [Float],
                             strides: (Int, Int) = (1,1),

aca9e4457209b58de05cc93c24df231223715abc
William Vabrinskas <wvabrinskas@gmail.com>
2025-07-15 19:41:17 -0400

    added dispatch group methods

--- Sources/NumSwift/Extensions.swift
+++ Sources/NumSwift/Extensions.swift
@@ -9,6 +9,67 @@ import Foundation
 
 //Courtesy of: https://github.com/palle-k/DL4S-WGAN-GP/blob/master/Sources/WGANGP/Util.swift
 extension DispatchQueue {
+  static func concurrentBatchedPerform(units: Int,
+                                       workers: Int,
+                                       priority: DispatchQoS.QoSClass = .default,
+                                       task: @escaping (_ indexRange: CountableRange<Int>,
+                                                        _ workerIndex: Int,
+                                                        _ processingCount: Int,
+                                                        _ workerId: UUID) -> ()) {
+    
+    let sema = DispatchSemaphore(value: 0)
+    let tasksPerWorker = units / workers
+        
+    for workerID in 0 ..< workers {
+      let taskRange: CountableRange<Int>
+      if workerID == workers - 1 {
+        taskRange = tasksPerWorker * workerID ..< units
+      } else {
+        taskRange = tasksPerWorker * workerID ..< tasksPerWorker * (workerID + 1)
+      }
+      
+      let workerUuid = UUID()
+      
+      DispatchQueue.global(qos: priority).async {
+        task(taskRange, workerID, taskRange.count, workerUuid)
+
+        sema.signal()
+      }
+    }
+    
+    for _ in 0 ..< workers {
+      sema.wait()
+    }
+  }
+  
+  static func concurrentPerform(units: Int, workers: Int, priority: DispatchQoS.QoSClass = .default, task: @escaping (_ index: Int, _ processingCount: Int, _ workerId: UUID) -> ()) {
+    let sema = DispatchSemaphore(value: 0)
+    let tasksPerWorker = units / workers
+        
+    for workerID in 0 ..< workers {
+      let taskRange: CountableRange<Int>
+      if workerID == workers - 1 {
+        taskRange = tasksPerWorker * workerID ..< units
+      } else {
+        taskRange = tasksPerWorker * workerID ..< tasksPerWorker * (workerID + 1)
+      }
+      
+      let workerUuid = UUID()
+      
+      DispatchQueue.global(qos: priority).async {
+        for unitID in taskRange {
+          task(unitID, taskRange.count, workerUuid)
+        }
+        
+        sema.signal()
+      }
+    }
+    
+    for _ in 0 ..< workers {
+      sema.wait()
+    }
+  }
+  
   static func concurrentPerform(units: Int, workers: Int, priority: DispatchQoS.QoSClass = .default, task: @escaping (Int) -> ()) {
     let sema = DispatchSemaphore(value: 0)
     let tasksPerWorker = units / workers
